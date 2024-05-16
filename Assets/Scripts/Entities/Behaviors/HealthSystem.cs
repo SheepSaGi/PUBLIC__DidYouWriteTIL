355d0bd99b36d7608ff150c7e80ecee8f478515e
amor1523 <amor152394@gmail.com>
2024-05-16 20:59:51 +0900

    [Init} 초기 필요 스크립트 작성

---
+++ Assets/Scripts/Entities/Behaviors/HealthSystem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private float healthChangeDelay = .5f;
+
+    private CharacterStatHandler statsHandler;
+    private float timeSinceLastChange = float.MaxValue;
+    private bool isAttacked = false;
+
+    // 체력이 변했을 때 할 행동들을 정의하고 적용 가능
+    public event Action OnDamage;
+    public event Action OnHeal;
+    public event Action OnDeath;
+    public event Action OnInvincibilityEnd;
+
+    public float CurrentHealth { get; private set; }
+
+    // get만 구현된 것처럼 프로퍼티를 사용하는 것
+    // 이렇게 하면 데이터의 복제본이 여기저기 돌아다니다가 싱크가 깨지는 문제를 막을 수 있어요!
+    public float MaxHealth => statsHandler.CurrentStat.maxHealth;
+
+
+}
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Transform Player { get; private set; }
+    // public ObjectPool ObjectPool { get; private set; }
+    [SerializeField] private string playerTag = "Player";
+
+    private void Awake()
+    {
+        Instance = this;
+        Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        // ObjectPool = GetComponent<ObjectPool>();
+    }
+}
