45d6e4df4127fb9440ed269f5a49b34ec2ca38aa
Kilsunho <sh9567@naver.com>
2024-05-20 17:22:48 +0900

    Fix : TILShoot
    TILShoot 오류 수정

--- Assets/Scripts/Shoot/TILShoot.cs
+++ Assets/Scripts/Shoot/TILShoot.cs
@@ -1,13 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using Unity.Properties;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+
 using UnityEngine.Pool;
 
 
 public class TILShoot : MonoBehaviour
 {
-    
+    private TILController controller;
+
+    private ObjectPool objectPool;
+
+    [SerializeField] private Transform bulletSpawnPosition;
+    private Vector2 aimDirection = Vector2.right;
+
+    public GameObject CharacterBulletPrefab;
+
+    private void Awake()
+    {
+        controller = GetComponent<TILController>();
+        objectPool = GetComponent<ObjectPool>();
+    }
+
+    private void Start()
+    {
+        controller.OnAttackEvent += Onshoot;
+    }
+    //private void OnAim(Vector2 newAimDirection)
+    //{
+    //    aimDirection = newAimDirection;
+    //}
+
+    private void Onshoot(AttackSO sO)
+    {
+        CreateProjectile();    
+    }
+
+    private void CreateProjectile()
+    {
+
+        GameObject obj = objectPool.SpawnFromPool("Arrow");
+        obj.transform.position = bulletSpawnPosition.position;
+        BulletController attackController = obj.GetComponent<BulletController>();
+
+        //Instantiate(CharacterBulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
+    }
+
+    private Vector2 RotateVector2(Vector2 v)
+    {
+        return Quaternion.Euler(0, 0, 0) * v;
+    }
 }
