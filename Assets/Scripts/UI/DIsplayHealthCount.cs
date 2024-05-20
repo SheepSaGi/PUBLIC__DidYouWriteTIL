4b98add19993e1a2476feac64083d8055362a38b
ss-zun <zxq0158@gmail.com>
2024-05-20 19:08:06 +0900

    Update : DIsplayHealthCount

--- Assets/Scripts/UI/DIsplayHealthCount.cs
+++ Assets/Scripts/UI/DIsplayHealthCount.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class DIsplayHealthCount : MonoBehaviour
 {
     [SerializeField] private HealthSystem characterHealth;
+    private ObjectPool objectPool;
+    private Stack<GameObject> healthUIs;
 
+    private void Awake()
+    {
+        //3에 characterHealth.CurrentHealth 넣을 예정
+        foreach (var healthUI in healthUIs)
+        {
+            healthUI.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        characterHealth.OnDamage += DisableHealthCount;
+    }
+
+    private void DisableHealthCount()
+    {
+
+    }
 }
