fba89bbdd8bb156d24a89046febe6746144b9c2e
amor1523 <amor152394@gmail.com>
2024-05-20 19:25:44 +0900

    [Complex] 플레이어 Prefab 등 수정
    플레이어 Prefab
    적 Prefab
    UI 수정
    스크립트 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public Transform Player { get; private set; }
-    // public ObjectPool ObjectPool { get; private set; }
+    public ObjectPool ObjectPool { get; private set; }
     [SerializeField] private string playerTag = "Player";
 
     private void Awake()
@@ -16,6 +16,6 @@ public class GameManager : MonoBehaviour
         Instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
 
-        // ObjectPool = GetComponent<ObjectPool>();
+        ObjectPool = GetComponent<ObjectPool>();
     }
 }
--- Assets/Scripts/UI/DIsplayHealthCount.cs
+++ Assets/Scripts/UI/DIsplayHealthCount.cs
@@ -12,15 +12,15 @@ public class DIsplayHealthCount : MonoBehaviour
     private void Awake()
     {
         //3에 characterHealth.CurrentHealth 넣을 예정
-        foreach (var healthUI in healthUIs)
-        {
-            healthUI.SetActive(false);
-        }
+        //foreach (var healthUI in healthUIs)
+        //{
+        //    healthUI.SetActive(false);
+        //}
     }
 
     private void Update()
     {
-        characterHealth.OnDamage += DisableHealthCount;
+        //characterHealth.OnDamage += DisableHealthCount;
     }
 
     private void DisableHealthCount()
