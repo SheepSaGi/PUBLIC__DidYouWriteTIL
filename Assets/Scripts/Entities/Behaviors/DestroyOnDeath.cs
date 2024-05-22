e58b825a1a4ae0c3545cd089e1285a8fc58e65b8
amor1523 <amor152394@gmail.com>
2024-05-22 10:21:10 +0900

    [Fix] DestroyOnDeath 스크립트 변수수정

--- Assets/Scripts/Entities/Behaviors/DestroyOnDeath.cs
+++ Assets/Scripts/Entities/Behaviors/DestroyOnDeath.cs
@@ -5,15 +5,14 @@ public class DestroyOnDeath : MonoBehaviour
     private HealthSystem healthSystem;
     private Rigidbody2D rigidbody;
     private GameManager gameManager;
-    
-    private AudioSource enemyDeath;
+    private AudioSource effect;
     public AudioClip explodeClip;
 
     private void Start()
     {
         healthSystem = GetComponent<HealthSystem>();
         rigidbody = GetComponent<Rigidbody2D>();
-        enemyDeath = GetComponent<AudioSource>();
+        effect = GetComponent<AudioSource>();
         // 실제 실행 주체는 healthSystem임
         healthSystem.OnDeath += OnDeath;
         gameManager = FindObjectOfType<GameManager>();
@@ -38,8 +37,7 @@ public class DestroyOnDeath : MonoBehaviour
         if (gameObject.CompareTag("Enemy"))
         {
             Debug.Log("효과음발생");
-            //enemyDeath.Play();
-            enemyDeath.PlayOneShot(explodeClip);
+            effect.PlayOneShot(explodeClip);
         }
 
         // 스크립트 더이상 작동 안하도록 함
